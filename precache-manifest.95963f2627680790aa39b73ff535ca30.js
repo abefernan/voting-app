113a73b0c26fa9999a803dbca92f8ea8b52d682a
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-08-19 00:03:30 +0200

    Updates

---
+++ precache-manifest.95963f2627680790aa39b73ff535ca30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6add84de9378a1c4f493933aa0a759b3",
+    "url": "/voting-app/index.html"
+  },
+  {
+    "revision": "6553b6ce56597ed72dd0",
+    "url": "/voting-app/static/css/main.f319df4c.chunk.css"
+  },
+  {
+    "revision": "69c7034a7cddcef2638d",
+    "url": "/voting-app/static/js/2.c83c6b31.chunk.js"
+  },
+  {
+    "revision": "047ba091534625a67c670a6626faf8d1",
+    "url": "/voting-app/static/js/2.c83c6b31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6553b6ce56597ed72dd0",
+    "url": "/voting-app/static/js/main.0b61f7af.chunk.js"
+  },
+  {
+    "revision": "4fc2fe940ecfc033ab4e",
+    "url": "/voting-app/static/js/runtime-main.b9137f1f.js"
+  }
+]);
